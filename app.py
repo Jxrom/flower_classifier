1b347688249ff88e84dab42c81ed3a46fbc54e2b
Jxrom <68335788+Jxrom@users.noreply.github.com>
2023-05-14 17:36:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,33 +3,48 @@ import tensorflow as tf
 from PIL import Image, ImageOps
 import numpy as np
 
-@st.cache_resource
-def load_model():
-    model = tf.keras.models.load_model('flower_classifier.hdf5')
-    return model
+def main():
+    st.title("Streamlit App")
+    st.markdown(
+        """
+        <style>
+        body {
+            background: linear-gradient(to bottom, #0074D9, #1E90FF); 
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
 
-def import_and_predict(image_data, model):
-    size = (128, 128)
-    image = ImageOps.fit(image_data, size, Image.LANCZOS)
-    img = np.asarray(image)
-    img_reshape = img[np.newaxis, ...]
-    prediction = model.predict(img_reshape)
-    return prediction
+    @st.cache_resource
+    def load_model():
+        model = tf.keras.models.load_model('flower_classifier.hdf5')
+        return model
 
-model = load_model()
-class_names = ["Daisy", "Dandelion", "Rose", "Sunflower", "Tulip"]
+    def import_and_predict(image_data, model):
+        size = (128, 128)
+        image = ImageOps.fit(image_data, size, Image.LANCZOS)
+        img = np.asarray(image)
+        img_reshape = img[np.newaxis, ...]
+        prediction = model.predict(img_reshape)
+        return prediction
 
-st.write("# Flower Type Classifier Developed By Jerome Marbebe")
+    model = load_model()
+    class_names = ["Daisy", "Dandelion", "Rose", "Sunflower", "Tulip"]
 
-file = st.file_uploader("Choose plant photo from computer", type=["jpg", "png"])
+    st.write("# Flower Type Classifier Developed By Jerome Marbebe")
 
-if file is None:
-    st.text("Please upload an image file")
-else:
-    image = Image.open(file)
-    st.image(image, use_column_width=True)
-    prediction = import_and_predict(image, model)
-    class_index = np.argmax(prediction)
-    class_name = class_names[class_index]
-    string = "OUTPUT: " + class_name
-    st.success(string)
+    file = st.file_uploader("Choose plant photo from computer", type=["jpg", "png"])
+
+    if file is None:
+        st.text("Please upload an image file")
+    else:
+        image = Image.open(file)
+        st.image(image, use_column_width=True)
+        prediction = import_and_predict(image, model)
+        class_index = np.argmax(prediction)
+        class_name = class_names[class_index]
+        string = "OUTPUT: " + class_name
+        st.success(string)
+ if __name__ == "__main__":
+    main()
